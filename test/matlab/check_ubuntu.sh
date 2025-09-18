c9e1535c8f4dac524eceddf53fc288cc036c6941
Prabhakar Kumar <prabhakk@mathworks.com>
2025-09-18 19:45:05 +0530

    Update tests to use 25a until matlab engine for python releases support for 25b

--- test/matlab/check_ubuntu.sh
+++ test/matlab/check_ubuntu.sh
@@ -4,7 +4,7 @@
 #-------------------------------------------------------------------------------------------------------------
 #
 # This test file will be executed against one of the scenarios devcontainer.json test that
-# includes the 'matlab' feature with the R2025b release, and a support package installed.
+# includes the 'matlab' feature with the R2025a release, and a support package installed.
 # Support package installation is special, because these packages need to be installed into
 # the end users HOME folder and not into the root users folders. Installing into root will
 # result in users being unable to access the Support Packages.
@@ -29,10 +29,10 @@ source dev-container-features-test-lib
 # check <LABEL> <cmd> [args...]
 
 # Verify that the right release is installed in the expected location.
-check "R2025b is installed" bash -c "cat /opt/matlab/R2025b/VersionInfo.xml | grep '<release>R2025b</release>'"
+check "R2025a is installed" bash -c "cat /opt/matlab/R2025a/VersionInfo.xml | grep '<release>R2025a</release>'"
 
 # Verify MATLAB_Support_Package_for_Android_Sensors is installed at the right place (ie: The home folder for the containerUser : vscode )
-check "support package is installed" bash -c "cat /home/vscode/Documents/MATLAB/SupportPackages/R2025b/ssiSearchFolders | head -1 | grep 'toolbox/matlab/hardware/shared/hwsdk'"
+check "support package is installed" bash -c "cat /home/vscode/Documents/MATLAB/SupportPackages/R2025a/ssiSearchFolders | head -1 | grep 'toolbox/matlab/hardware/shared/hwsdk'"
 
 check "is startInDesktop marker file present" bash -c "ls ~/.teststartmatlabdesktop"
 
